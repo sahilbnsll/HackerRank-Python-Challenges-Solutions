08ee46cc15090bf83de92ac6101588a021ca0cf5
Sahil Bansal <bansalsahil740@gmail.com>
2021-07-14 22:46:22 +0530

    Text Wrap

--- Problem22.py
+++ Problem22.py
@@ -44,10 +44,16 @@ YZ
 '''
 
 
+import textwrap
 
+def wrap(string, max_width):
+    return '\n'.join(textwrap.wrap(string, max_width))
 
-
-
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
+    
 
 
 
